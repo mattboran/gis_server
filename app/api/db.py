009b12bb8809a6def3ec97d51969f3a46f6a7392
Matt Boran <mattboran@gmail.com>
2020-12-28 12:49:57 -0700

    Move db location

--- app/api/db.py
+++ app/api/db.py
@@ -1,6 +1,10 @@
+import os
+
 from peewee import SqliteDatabase
 
-db = SqliteDatabase('database.db', pragmas={
+path = os.path.join(os.getcwd(), 'gis_data', 'database.db')
+
+db = SqliteDatabase(path, pragmas={
     'journal_mode': 'wal',
     'cache_size': -1024 * 64
 })
--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,13 +1,17 @@
+import os
+
 from fastapi import Security, HTTPException
 from fastapi.security.api_key import APIKeyQuery
 from starlette.status import HTTP_403_FORBIDDEN
+
 
 API_KEY_NAME = 'token'
 
 api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
 
 try:
-    with open ('/app/.env', 'r') as env_file:
+    path = os.path.join(os.getcwd(), '.env')
+    with open (path, 'r') as env_file:
         lines = env_file.readlines()
         env = {l.split('=')[0]: l.split('=')[1] for l in lines}
 except FileNotFoundError:
