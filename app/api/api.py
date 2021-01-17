192852c3c145fcf34b9ae9eb54219b4ee279f007
Matt Boran <mattboran@gmail.com>
2021-01-16 19:28:37 -0700

    Add Shapely dependency, basically nuke the buckets endpoint

--- app/api/api.py
+++ app/api/api.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from shapely.geometry import Point
 
 import api.geometry as geom
 from .dependencies import get_token
@@ -22,23 +23,6 @@ class CoordinateOut(BaseModel):
 class PointOut(BaseModel):
     x: float
     y: float
-
-
-class BucketResult(BaseModel):
-    idx: int
-    address: str
-    height: Optional[float]
-    ground_elevation: Optional[float]
-    building_type: Optional[str]
-    center: CoordinateOut
-    origin: CoordinateOut
-    polygon: List[PointOut]
-    polygon_coords: List[CoordinateOut]
-
-
-class BucketOut(BaseModel):
-    count: int
-    result: List[BucketResult]
 
 
 class AddressResult(BaseModel):
@@ -73,42 +57,17 @@ class IntersectionOut(BaseModel):
     result: List[IntersectionResult]
 
 
-@router.get('/bucket', response_model=BucketOut)
-async def get_bucket(region: str, lat: float, lon: float):
-    bucket = Bucket.get(Bucket.region == region)
-    indices = bucket.indices_surrounding_coordinate((lon, lat))
-    buildings = list(set(Building.get_buildings_for_bucket_indices(indices)))
-    result = []
-    for b in buildings:
-        c_lon, c_lat = b.address.coord[0]
-        o_lon, o_lat = b.origin
-        poly_coords = [CoordinateOut(latitude=p[1], longitude=p[0]) for p in b.polygon_points]
-        out = BucketResult(idx=b.idx,
-                           address=b.address.full_address,
-                           height=b.height * geom.FT_TO_M,
-                           ground_elevation=b.ground_elevation * geom.FT_TO_M,
-                           building_type=b.building_type,
-                           center=CoordinateOut(latitude=c_lat, longitude=c_lon),
-                           origin=CoordinateOut(latitude=o_lat, longitude=o_lon),
-                           polygon=[PointOut(x=p[0], y=p[1]) for p in b.points_in_local_coords],
-                           polygon_coords=poly_coords)
-        result.append(out.dict())
-    return {
-        'count': len(result),
-        'result': result
-    }
-
-@router.get('/addresses', response_model=AddressOut)
-async def get_addresses(region: str, lat: float, lon: float):
-    indices = (Bucket.get(Bucket.region == region)
-                     .indices_surrounding_coordinate((lon, lat)))
-    addresses = (Address.select(Address.predirective, Address.address_1, Address.street_name, Address.post_type, Address.region,
-                                Address.coord, Address.building_idx, Address.street_idx, Building.idx, Building.polygon_points,
-                                Street.idx, Street.coords)
+def addresses_for_indices(indices):
+    return (Address.select(Address.predirective, Address.address_1, Address.street_name, 
+                           Address.post_type, Address.region, Address.coord, 
+                           Address.building_idx, Address.street_idx, Building.idx, 
+                           Building.polygon_points, Street.idx, Street.coords)
                         .where(Address.bucket_idx << indices)
                         .join(Building, attr='building', on=(Building.idx == Address.building_idx))
                         .switch(Address)
                         .join(Street, attr='street', on=(Street.idx == Address.street_idx)))
+
+def address_dict(addresses):
     result = dict()
     for address in addresses:
         predirective = f" {address.predirective} " if address.predirective else ""
@@ -117,13 +76,18 @@ async def get_addresses(region: str, lat: float, lon: float):
         coord = CoordinateOut(latitude=address.coord[0][1], longitude=address.coord[0][0])
         polygon_coords = [CoordinateOut(latitude=p[1], longitude=p[0]) for p in address.building.polygon_points]
         street_coords = [CoordinateOut(latitude=p[1], longitude=p[0]) for p in address.street.coords if address.street]
-        addr = AddressResult(address=full_address, coord=coord, polygon_coords=polygon_coords, street_coords=street_coords)
+        addr = AddressResult(address=full_address,
+                            coord=coord,
+                            polygon_coords=polygon_coords,
+                            street_coords=street_coords)
         if full_address in result:
             result[full_address].append(addr)
         else:
             result[full_address] = [addr]
-    
-    for full_address, address_models in result.items():
+    return result
+
+def unique_address_dict(address_dict):
+    for full_address, address_models in address_dict.items():
         coords = [addr.coord for addr in address_models]
         # The most common polygon is most likely the correct one:
         try:
@@ -135,7 +99,19 @@ async def get_addresses(region: str, lat: float, lon: float):
         longitude = sum([c.longitude for c in coords]) / float(len(coords))
         coord = CoordinateOut(latitude=latitude, longitude=longitude)
         street_coords = address_models[0].street_coords
-        result[full_address] = AddressResult(address=full_address, coord=coord, polygon_coords=polygon, street_coords=street_coords)
+        address_dict[full_address] = AddressResult(address=full_address, 
+                                                   coord=coord,
+                                                   polygon_coords=polygon,
+                                                   street_coords=street_coords)
+    return address_dict
+
+@router.get('/addresses', response_model=AddressOut)
+async def get_addresses(region: str, lat: float, lon: float):
+    indices = (Bucket.get(Bucket.region == region)
+                     .indices_surrounding_coordinate((lon, lat)))
+    addresses = addresses_for_indices(indices)
+    result = address_dict(addresses)
+    result = unique_address_dict(result)
     return {
         'count': len(result),
         'result': list(result.values())
